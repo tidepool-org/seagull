e7bf8b759dbe99c99c6d4ff90defc4ee4e6777d9
Jamie <jamie@tidepool.org>
2016-01-21 15:24:27 +1300

    consistent testing connection string

--- test/testMongoCrudHandler.js
+++ test/testMongoCrudHandler.js
@@ -25,7 +25,7 @@ var expect = require('salinity').expect;
 /* jshint expr: true */
 
 var metadb = require('../lib/mongoCrudHandler.js')({
-  mongoConnectionString: 'mongodb://localhost/seagulltest',
+  mongoConnectionString: 'mongodb://localhost/test_seagull',
   // the special config value we pass for testing will enable us to wipe the database
   _wipeTheEntireDatabase: true,
   adminKey: 'specialkey',
