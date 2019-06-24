158b2a4a0c21299ce758b5c60cef31c67ad5b2c4
Derrick Burns <derrickrburns@gmail.com>
2019-06-24 00:32:52 -0700

    Added mongo params.

--- env.js
+++ env.js
@@ -20,6 +20,7 @@
 
 var fs = require('fs');
 var config = require('amoeba').config;
+var cs = require('amoeba').toConnectionString;
 
 function maybeReplaceWithContentsOfFile(obj, field)
 {
@@ -55,7 +56,7 @@ module.exports = (function() {
     throw new Error('Must specify either PORT or HTTPS_PORT in your environment.');
   }
 
-  env.mongoConnectionString = config.fromEnvironment('MONGO_CONNECTION_STRING', 'mongodb://localhost/seagull');
+  env.mongoConnectionString = cs('seagull')
 
   // Configurable salt for password encryption
   env.saltDeploy = config.fromEnvironment('SALT_DEPLOY');
